9d5a9f009a53b2ae782405310d1973e186e36e51
Samuel Garcia <sam.garcia.die@gmail.com>
2020-01-06 12:20:44 +0100

    make index

--- conf.py
+++ conf.py
@@ -225,12 +225,14 @@ POSTS = (
     ("posts/*.md", "posts", "post.tmpl"),
     ("posts/*.txt", "posts", "post.tmpl"),
     ("posts/*.html", "posts", "post.tmpl"),
+    ("posts/*.ipynb", "blog", "post.tmpl"), # samuel garcia for jupyter notbook
 )
 PAGES = (
-    ("pages/*.rst", "pages", "page.tmpl"),
-    ("pages/*.md", "pages", "page.tmpl"),
-    ("pages/*.txt", "pages", "page.tmpl"),
-    ("pages/*.html", "pages", "page.tmpl"),
+    ("pages/*.rst", "", "page.tmpl"),
+    ("pages/*.md", "", "page.tmpl"),
+    ("pages/*.txt", "", "page.tmpl"),
+    ("pages/*.html", "", "page.tmpl"),
+    ("pages/*.ipynb", "", "page.tmpl"), # samuel garcia for jupyter notbook
 )
 
 
@@ -546,6 +548,7 @@ HIDDEN_AUTHORS = ['Guest']
 # output / TRANSLATION[lang] / INDEX_PATH / index-*.html
 # (translatable)
 # INDEX_PATH = ""
+INDEX_PATH = "blog"
 
 # Optional HTML that displayed on “main” blog index.html files.
 # May be used for a greeting. (translatable)
@@ -1299,7 +1302,8 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.c
 
 # Special settings to disable only parts of the indexes plugin.
 # Use with care.
-# DISABLE_INDEXES = False
+# DISABLE_INDEXES = False
+# DISABLE_INDEXES = True
 # DISABLE_MAIN_ATOM_FEED = False
 # DISABLE_MAIN_RSS_FEED = False
 
--- pages/index.rst
+++ pages/index.rst
@@ -1,4 +1,4 @@
-.. title: index
+.. title: Welcome to spikeinterface report
 .. slug: index
 .. date: 2020-01-06 11:37:28 UTC+01:00
 .. tags: 
@@ -9,15 +9,30 @@
 
 
 .. image:: /images/logo.png
-   :align: right
    :height: 200 pt
 
-spikeinterface report is a collection of notebook that :
+**spikeinterface report** is a collection of notebooks that :
 
-  * demonstrate what can be done with the spiekinetrface tool
-  * show detailed benchmark for spike sorter
+  * demonstrate what can be done with the spikeinterface tool
+  * show detailed benchmarks for spike sorter
   * serve as a sandbox for future benchmark that can be integrated in spikeforest
-  * test some idea 
+  * test some idea
+
+Some ideas we want to test here:
+
+  * compute agreement accross sorter for none groundth truth dataset
+  * make benchmark for sorters that separate:
+  
+     * the accuracy per GT units
+     * how many units are detected
+    
+    Actual benchmarks make an average of accuracy that mix theses metrics
+    so a sorter with high accuracy but that do not detect a cell will have a low
+    average accuracy.
+    
+  * make benchmark for spike collision
+  * make benchmark for drift
+  * test if "agreement sorting" is relevant
 
 
-
+  
